4a143506d3c3fbedc9e7f753b32d4bce78db7866
WeisserSchatten <aeliseey@bk.ru>
2018-04-25 23:39:35 +0300

    nedodelano

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -34,83 +34,62 @@ $(document).ready(function () {
 		$('.model-sofa').toggleClass('-invisible');
 		$('.sofa-text').toggleClass('-hover-text');
 	});
-
-
-
-
-
-	$('.options-sofa-img').click(function() {
-		$('.options-sofa-img').addClass('-invisible');
-		$('.sofa-options-second').addClass('-visible');
-		$('.options-sofa-img-hover').addClass('-visible');
-	});
-	$('.options-carpet-img').click(function() {
-		$('.options-carpet-img').addClass('-invisible');
-		$('.carpet-options-second').addClass('-visible');
-		$('.options-carpet-img-hover').addClass('-visible');
-	});
-	$('.options-mattress-img').click(function() {
-		$('.options-mattress-img').addClass('-invisible');
-		$('.mattress-options-second').addClass('-visible');
-		$('.options-mattress-img-hover').addClass('-visible');
-	});
-	$('.options-chair-img').click(function() {
-		$('.options-chair-img').addClass('-invisible');
-		$('.chair-options-second').addClass('-visible');
-		$('.options-chair-img-hover').addClass('-visible');
+ 
+	
+	$('.js-minus').click(function () { 
+		var $input = $(this).parent().find('.js-input-quantity'); 
+		var count = parseInt($input.val()) - 1; 
+		count = count < 1 ? 1 : count; 
+		$input.val(count); 
+		$input.change(); 
+		return false; 
+	}); 
+	$('.js-plus').click(function () { 
+		var $input = $(this).parent().find('.js-input-quantity'); 
+		$input.val(parseInt($input.val()) + 1); 
+		$input.change(); 
+		return false; 
 	});
 
+	xCal("date8", {
+		id: "cssworldru8", 
+		"class": "xcalend",
+		hide: 0,
+		x: 0,
+		autoOff: 0, 
+		to: "date8" 
+	});
 
 
-	function FirstParameterSofa() {
-		$('.options-sofa-img').removeClass('-invisible');
-		$('.sofa-options-second').removeClass('-visible');
-		$('.options-sofa-img-hover').removeClass('-visible');
-	};
-	function SecondParameterSofa() {
-		$('.options-sofa-img').removeClass('-invisible');
-		$('.sofa-options-first').removeClass('-visible');
-		$('.options-sofa-img-hover').removeClass('-visible');
-	};
+	///////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////
+	let priceBean = 100;
+	let priceSofa = 500;
+	let priceMattress = 300;
+	let priceChair = 200;
+	let priceCouch = 350;
+	let priceCarpet = 150;
+	let priceArmchair = 250;
 
-	function FirstParameterChair() {
-		$('.options-chair-img').removeClass('-invisible');
-		$('.chair-options-second').removeClass('-visible');
-		$('.options-chair-img-hover').removeClass('-visible');
-	};
-	function SecondParameterChair() {
-		$('.options-chair-img').removeClass('-invisible');
-		$('.chair-options-first').removeClass('-visible');
-		$('.options-chair-img-hover').removeClass('-visible');
-	}
-
-	function FirstParameterCarpet() {
-		$('.options-carpet-img').removeClass('-invisible');
-		$('.carpet-options-second').removeClass('-visible');
-		$('.options-carpet-img-hover').removeClass('-visible');
-	}
-	function SecondParameterCarpet() {
-		$('.options-carpet-img').removeClass('-invisible');
-		$('.carpet-options-first').removeClass('-visible');
-		$('.options-carpet-img-hover').removeClass('-visible');
-	}
-
-	function FirstParameterMattress () {
-		$('.options-mattress-img').removeClass('-invisible')
-		$('.mattress-options-second').removeClass('-visible')
-		$('.options-mattress-img-hover').removeClass('-visible');
-	}
-	function SecondParameterMattress () {
-		$('.options-mattress-img').removeClass('-invisible');
-		$('.mattress-options-first').removeClass('-visible');
-		$('.options-mattress-img-hover').removeClass('-visible');
-	}
-
-
-
-	$('.options-mattress-img-hover').click(FirstParameterMattress)
-	$('.mattress-options-first').click(FirstParameterMattress())
-	$('.mattress-options-second').click( SecondParameterMattress() );
-
-
+	$('.model-couch-mark').click(function() {
+		
+	});
+	$('.model-armchair-mark').click(function() {
+		
+	});
+	$('.model-mattress-mark').click(function() {
+		
+	});
+	$('.model-chair-mark').click(function() {
+		
+	});
+	$('.model-carpet-mark').click(function() {
+		
+	});
+	$('.model-bean-mark').click(function() {
+		
+	});
+	$('.model-sofa-mark').click(function() {
+		
+	});
 });
